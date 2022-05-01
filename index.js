1b53dd5d685caab48f40238f963686e85def80ee
Stephen Oyeyemi <94493468+stevesdiary@users.noreply.github.com>
2022-05-01 17:55:49 +0100

    changed the prev, play/pause and next btn variable names

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ let trackArt = document.querySelector('.track-art');
 let trackName = document.querySelector('.track-name');
 let trackArtist = document.querySelector('.track-artist');
 
-let prevTrack = document.querySelector('.prev-track');
-let playpauseTrack = document.querySelector('.playpause-track');
-let nextTrack = document.querySelector('.next-track');
+let prevBtn = document.querySelector('.prev-track');
+let playpauseBtn = document.querySelector('.playpause-track');
+let nextBtn = document.querySelector('.next-track');
 
 let currentTime = document.querySelector('.current-time');
 let totalDuration = document.querySelector('.total-duration');
@@ -21,9 +21,9 @@ let currentTrack = document.createElement('audio');
 
 let trackList = [
    {
-      name: 'Majesty',
-      artist:'Michael Smith',
-      image:'/Assets/images/images (11) (3).jpeg',
+      name: "Majesty",
+      artist:"Michael Smith",
+      image:"/Assets/images/imageMajesty.jpeg",
       path: "/Assets/songs/Michael Smith - Majesty.ogg"
    },
    {
@@ -51,3 +51,15 @@ let trackList = [
       path:"/Assets/songs/Hillsong - Still.mp3"
    }
 ]
+
+function loadTrack(track_index);
+   clearInterval(updateTimer);
+   resetValues()
+
+   currentTrack.src= trackList[track_index].path;
+   currentTrack.load();
+
+   trackArt.style.backgroundImage = "url("+ trackList[track_index].image + ")";
+   trackName.textContent = trackList[track_index].name;
+   trackArtist.textContent = trackList[track_index].artist;
+   nowPlaying.textContent = "PLAYING " + (track_index + 1 )  + " OF " + (trackList.length);
